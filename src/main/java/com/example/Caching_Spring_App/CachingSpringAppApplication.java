0748f1b5212dd37464a2e4fa995d42b924a50418
asthasingh2445@gmail.com <asthasingh2445@gmail.com>
2023-05-09 23:25:50 +0530

    Initial commit

---
+++ src/main/java/com/example/Caching_Spring_App/CachingSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Caching_Spring_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Caching_Spring_App/CachingSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Caching_Spring_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
